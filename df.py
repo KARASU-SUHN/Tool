6328bd7801a4608f9ea5137f83472e106aa25bdf
KARASU-SUHN <43245579+KARASU-SUHN@users.noreply.github.com>
2022-12-19 17:03:17 +0900

    Update df.py

--- df.py
+++ df.py
@@ -29,8 +29,21 @@ mv example1.txt example2.txt #rename file name in Linux
 
 cat <filepath> #command for open a file in linux
 
-
+# resize figure
 from PIL import Image
 image = Image.open('path.jpeg')
 new_image = image.resize((240, 320)) #resize
 new_image.save('...jpeg')
+
+
+#merge df by same column name
+dfs = [df1, df2, df3, df4..]
+c = pd.concat(dfs, join='outer', axis = 0) #0-index 1-column
+# merge by different column
+from functools import reduce
+data_frames = [df1, df2, df3]
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['DATE'],
+                                            how='outer'), data_frames)
+# if you want to fill the values that don't exist in the lines of merged dataframe simply fill with required strings as
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['DATE'],
+                                            how='outer'), data_frames).fillna('void')
