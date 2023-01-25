4942e172ce2bc06a7875bf5ead502578932b0ada
KARASU-SUHN <43245579+KARASU-SUHN@users.noreply.github.com>
2023-01-25 16:18:46 +0900

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#create scatter plot
+plt.scatter(df.x, df.y)
+
+#add horizontal line at mean value of y
+plt.axhline(y=np.nanmean(df.y), color='red', linestyle='--', linewidth=3, label='Avg')
+
+#add vertcal line at median value of y
+plt.axvline(x=np.nanmedian(df.y), color='red', linestyle='--', linewidth=3, label='Avg')
