c1fb8aa631b07413c3632428508d8c1a5ef38657
KARASU-SUHN <43245579+KARASU-SUHN@users.noreply.github.com>
2023-02-01 22:07:03 +0900

    Update plot.py

--- plot.py
+++ plot.py
@@ -14,3 +14,22 @@ plt.axvline(x=np.nanmedian(df.y), color='red', linestyle='--', linewidth=3, labe
 ##matplotlib color
 https://matplotlib.org/stable/gallery/color/named_colors.html
 https://www.colordic.org/
+#seaborn plot color
+https://qiita.com/SaitoTsutomu/items/c79c9973a92e1e2c77a7
+#seaborn boxplot
+https://levelup.gitconnected.com/statistics-on-seaborn-plots-with-statannotations-2bfce0394c00
+
+states_palette = sns.color_palette("Set3", n_colors=6)
+# RdYlBu_r RdYlGn_r Set3
+sns.set(rc={'figure.figsize':(10,7)})
+sns.set_theme(style='whitegrid')
+ax = sns.boxplot(
+    x="year",
+    y="Magnet", 
+    data=domainM,
+    hue="domain",
+    showfliers=False,
+    palette=states_palette
+)
+l = plt.legend(loc='upper left', borderaxespad=0)
+plt.xticks(rotation=0)
